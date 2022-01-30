45761f71197dd1e5a50404eec03b4d06591e7c8a
CamilaNunes <cvn.camila@gmail.com>
2022-01-30 20:08:30 -0300

    configurando modal de criação;

--- cnmoney-app/src/App.tsx
+++ cnmoney-app/src/App.tsx
@@ -1,12 +1,27 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
 
 export function App() {
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTrasactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTrasactionModalOpen(false);  
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTrasactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>  
+      <Modal isOpen={isNewTrasactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Cadastrar Transação</h2>  
+      </Modal>
       <GlobalStyle/>
     </>
   );
--- cnmoney-app/src/components/Header/index.tsx
+++ cnmoney-app/src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from './styles';
+interface Headerprops{
+  onOpenNewTrasactionModal: () => void;
+}
 
-export function Header(){
+export function Header({ onOpenNewTrasactionModal }: Headerprops){
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="cn money" />
-        <button>Nova Transação</button>
+        <button type="button" onClick={onOpenNewTrasactionModal}>Nova Transação</button>
       </Content>
     </Container>
   )
