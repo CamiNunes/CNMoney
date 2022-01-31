1053f133313874602b10aa6e73275beb932870f5
CamilaNunes <cvn.camila@gmail.com>
2022-01-31 20:08:05 -0300

    Cores dos botões;

--- cnmoney-app/src/components/NewTransactionModal/index.tsx
+++ cnmoney-app/src/components/NewTransactionModal/index.tsx
@@ -22,11 +22,11 @@ export function NewTrasanctionModal({isOpen, onRequestClose}:NewTrasanctionModal
         <input placeholder="Título"/>
         <input type="number" placeholder="Valor"/>
         <TransactionTypeContainer>
-          <ButtonTypeTransaction type="button" onClick={ () => { setType('deposit');}} isActive={type === 'deposit'}>
+          <ButtonTypeTransaction type="button" onClick={ () => { setType('deposit');}} isActive={type === 'deposit'} activeColor="green" >
             <img src={incomeImg} alt="Entrada"/>
             <span>Entrada</span>
           </ButtonTypeTransaction>
-          <ButtonTypeTransaction type="button" onClick={ () => { setType('withdraw');}} isActive={type === 'withdraw'}>
+          <ButtonTypeTransaction type="button" onClick={ () => { setType('withdraw');}} isActive={type === 'withdraw'} activeColor="red" >
           <img src={outcomeImg} alt="Saída"/>
           <span>Saída</span>
           </ButtonTypeTransaction>
--- cnmoney-app/src/components/NewTransactionModal/styles.ts
+++ cnmoney-app/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2{
@@ -56,15 +56,21 @@ export const TransactionTypeContainer = styled.div`
 `;
 
 interface ButtonTypeTransactionProps{
-  isActive:boolean;
+  isActive: boolean;
+  activeColor: 'green' | 'red';
 }
+
+const colors = {
+  green: '#33cc95',
+  red:'#e52e4d'
+};
 
 export const ButtonTypeTransaction = styled.button<ButtonTypeTransactionProps>`
   height: 4rem;
   border: 1px solid #D7D7D7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#CCC' : 'transparent'};
+  background: ${(props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
   display: flex;
   align-items: center;
