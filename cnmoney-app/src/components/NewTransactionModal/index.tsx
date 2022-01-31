7d9604ea070d55a99f26941e99d6e4ab40b1170b
CamilaNunes <cvn.camila@gmail.com>
2022-01-31 19:25:22 -0300

    Criando botões TIPO;

--- cnmoney-app/src/components/NewTransactionModal/index.tsx
+++ cnmoney-app/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from '../NewTransactionModal/styles';
+import { Container, TransactionTypeContainer } from '../NewTransactionModal/styles';
 
 interface NewTrasanctionModalProps{
   isOpen: boolean;
@@ -17,6 +19,16 @@ export function NewTrasanctionModal({isOpen, onRequestClose}:NewTrasanctionModal
         <h2>Cadastrar Transação</h2>  
         <input placeholder="Título"/>
         <input type="number" placeholder="Valor"/>
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+          <img src={outcomeImg} alt="Saída"/>
+          <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
         <input placeholder="Categoria"/>
         <button type="submit">Cadastrar</button>
       </Container>
